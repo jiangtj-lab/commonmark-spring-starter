5115b59f3d03a532b6a520a4018cd73cbf753e09
jiangtj <116749895@qq.com>
2020-07-23 16:43:30 +0800

    Fix @EnableCommonmark

--- src/main/java/com/jiangtj/common/commonmarkspringstarter/EnableCommonmark.java
+++ src/main/java/com/jiangtj/common/commonmarkspringstarter/EnableCommonmark.java
@@ -2,10 +2,16 @@ package com.jiangtj.common.commonmarkspringstarter;
 
 import org.springframework.context.annotation.Import;
 
+import java.lang.annotation.*;
+
 /**
  * Created by MrTT (jiang.taojie@foxmail.com)
  * 2020/7/23.
  */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
 @Import({ CommonmarkConfiguration.class })
 public @interface EnableCommonmark {
 }
